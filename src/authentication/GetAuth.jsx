537f33985cda04174e3e7d3b2fd6a4beffddd03d
Gee Brain <admin@gozkybrain.com.ng>
2024-12-06 10:40:08 +0100

    App Documented

--- src/authentication/GetAuth.jsx
+++ src/authentication/GetAuth.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
-import Login from './login';
+import Login from './Login';
 import Register from './register';
 import Home from '../pages/Home';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
